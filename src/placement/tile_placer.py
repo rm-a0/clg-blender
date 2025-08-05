0472d66444862367d16fa0e417a2b9c30a8b4559
rm-a0 <michalrepcik9@gmail.com>
2025-08-05 14:57:39 +0100

    chore: add docstrings

--- src/placement/tile_placer.py
+++ src/placement/tile_placer.py
@@ -4,11 +4,18 @@ from ..core.models.grid import Grid
 from ..ui.properties import CLG_PG_tile
 
 class TilePlacer:
+    '''
+    Orchestrates the tile placement process, bridging the core logic with Blender's API.
+
+    'bl_tile' refers to the 'CLG_PG_TILE' property group containing metadata and object references inside Blender.
+    'tile' or 'core_tile' refers to the class in 'src/core' module containing metadata used for placing and differentiating tiles.
+    '''
     def __init__(self, grid: Grid, tile_collection: bpy.props.CollectionProperty):
         self.grid = grid
         self.tile_map = {tile.name: tile for tile in tile_collection}
 
     def copy_bl_tile(self, tile: CLG_PG_tile) -> bpy.types.Collection:
+        '''Creates a shallow copy of a bl_tile's objects as a new Blender collection'''
         new_collection = bpy.data.collections.new(f"{tile.name}_copy")
         bpy.context.scene.collection.children.link(new_collection)
 
@@ -20,17 +27,20 @@ class TilePlacer:
         return new_collection
     
     def hide_bl_tile(self, tile: CLG_PG_tile) -> None:
+        '''Hides a bl_tile's objects in the viewport and render'''
         for obj_ref in tile.objects:
             if obj_ref.object_ref:
                 obj_ref.object_ref.hide_viewport = True
                 obj_ref.object_ref.hide_render = True
 
     def hide_all_bl_tiles(self) -> None:
+        '''Hides all bl_tiles in the tile map in the viewport and render'''
         for tile in self.tile_map.values():
             self.hide_bl_tile(tile=tile)
 
     # TODO: Remove debugging print after expanding and finalizing the method
     def move_bl_tile(self, collection: bpy.types.Collection, x: float, y: float, z: float) -> None:
+        '''Moves bl_tile's collection to the specified position preserving relative objects positions'''
         if not collection.objects:
             print("No objects in collection")
             return
@@ -69,12 +79,12 @@ class TilePlacer:
             print(f"Moved object {obj.name} to new location: {obj.location}")
 
     def place_tiles(self) -> None:
+        '''Places tiles from grid inside a Blender scene converting them to bl_tiles'''
         bpy.ops.object.select_all(action='DESELECT')
         self.hide_all_bl_tiles()
 
         for row in range(self.grid.width):
             for col in range(self.grid.height):
-                # Core refers to tiles from the 'core' module and bl refers to the actual tiles inside blender
                 core_tile = self.grid.get_tile(row, col)
                 core_tile_name = core_tile.definition.name
 
